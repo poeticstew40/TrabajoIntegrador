f7832386d7b3e23ddc65a9e395b2b0fce7726f6a
poeticstew40 <nicolas_gigena@hotmail.es>
2023-04-02 19:47:50 -0300

    creacion de pronosticos
    implementacion de metodos

--- src/main/java/utn/tp/integrador/equipo7/Pronostico.java
+++ src/main/java/utn/tp/integrador/equipo7/Pronostico.java
@@ -1,4 +1,36 @@
 package utn.tp.integrador.equipo7;
 
 public class Pronostico {
+
+    private Partido partido;
+    private Equipo equipo;
+    private String resultado;
+
+    public Pronostico(Partido partido, Equipo equipo, String resultado) {
+        this.partido = partido;
+        this.equipo = equipo;
+        this.resultado = resultado;
+    }
+
+    public Partido getPartido() {
+        return partido;
+    }
+
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public String getResultado() {
+        return resultado;
+    }
+
+    public int puntos(){
+        //TODO incompleto
+        String resultadoReal = partido.resultado(equipo);
+        if (resultado.equals(resultadoReal)){
+            return 1;
+        }else
+            return 0;
+    }
+
 }
